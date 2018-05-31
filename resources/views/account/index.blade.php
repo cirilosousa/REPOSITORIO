2d2528abab0dcf07b5f4f328e70646d056e7e4d3
CiriloSousa <cirilosousa@gmail.com>
2018-05-31 13:57:17 +0100

    13:57 31/05/2018

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -4,13 +4,30 @@
 
 @section('content')
 
+
+@php
+    $test=App\Associate_member::where('main_user_id','=', $id)
+                              ->where('associated_user_id','=', Auth::user()->id)
+                              ->get();
+@endphp
+
+
+@if ($id==Auth::user()->id || $test->first()!=null )    
+
+    <a class="btn btn-xs btn-primary active emlinha" href="/accounts/{{$id}}">All accounts</a>   
+    <a class="btn btn-xs btn-primary active emlinha" href="/accounts/{{$id}}/opened">Opened Accounts</a>   
+    <a class="btn btn-xs btn-primary active emlinha" href="/accounts/{{$id}}/closed">Closed Accounts</a>   
+    <a class="btn btn-xs btn-success active emlinha" href="/accounts/{{$id}}/create">Create Account</a>  
+
     @if(count($accounts))
+
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>Account Code</th>
                     <th>Account type</th>
                     <th>Current balance</th>
+                    <th>Account Operations</th>
                 </tr>
             </thead>
         
@@ -21,6 +38,49 @@
                         <td>{{ $account->type() }}</td>
                         <td>{{ $account->current_balance }}</td>
                         <td>
+
+                            @php
+                                $movimentos=$account->movements;
+                            @endphp
+
+
+                            @if ($id==Auth::user()->id)
+                                
+                                @if ( $movimentos->first() == null )
+                                    <form action="/account/{{$account->id}}" method="POST" role="form" class="emlinha">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button class="btn btn-xs btn-danger active">Delete</button>
+                                    </form>
+
+                                @else
+                                    <form action="/account/{{$account->id}}" method="POST" role="form" class="emlinha">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+                                        <button class="btn btn-xs btn-danger disabled">Delete</button>
+                                    </form>
+                                @endif
+
+                                <form action="/account/{{$account->id}}" method="POST" role="form" class="emlinha">
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-xs btn-primary active">Edit</button>
+                                </form>             
+
+                                @if ($account->trashed())
+                                    <form action="/account/{{$account->id}}/reopen" method="POST" role="form" class="emlinha">
+                                    {{ csrf_field() }}
+                                    {{ method_field('PATCH') }}
+                                    <button class="btn btn-xs btn-danger active">Reopen</button>
+                                @else    
+                                    </form>
+                                    <form action="/account/{{$account->id}}/close" method="POST" role="form" class="emlinha">
+                                        {{ csrf_field() }}
+                                        {{ method_field('PATCH') }}
+                                        <button class="btn btn-xs btn-primary active">Close</button>
+                                    </form>
+                                @endif
+                            @endif    
+                        </td>    
                     </tr>
                 @endforeach
             </tbody>
@@ -29,5 +89,9 @@
     @else
         <h2>No accounts found</h2>
     @endif
+
+@else
+    <h2>You dont have permission to view this user accounts</h2>
+@endif
 
 @endsection
