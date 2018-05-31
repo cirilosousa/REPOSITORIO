6654ceab9c73c99791650a38845dbd91c89e1b85
PE60\marta <martacvcps@gmail.com>
2018-05-31 11:52:54 +0100

    movimentos (update incompleto)
    função update incompleta, falta validação dos dados, aguardo feedback do profe.

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Storage;
 
 class DocumentsController extends Controller
 {
    public function getfile($path) {
-   	return response()->file(storage_path('app/img/' . $path));
+   	return response()->file(storage_path('app/documents/' . $path));
    }
 }
--- app/Http/Controllers/MovementsController.php
+++ app/Http/Controllers/MovementsController.php
@@ -22,28 +22,82 @@ class MovementsController extends Controller
     return view('movements.index', compact('movements'));
     }
 
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'movement_category_id' => 'required',
+            'type' => 'required',
+            'date' => 'required|date|date_format:Y-m-d', //photo
+            'value' => 'required|numeric| min:0.05|max:5000',
+            'description' => 'string|min:0|max:255',
+        ]);
+    }
 
-    public function create(){
+
+    public function create(array $data){
+    	$movement = Movement::create([
+            'date' => $data['date'],
+            'movement_category_id' => $data['movement_category_id'],
+            'type' => $data['type'],
+            'date' => $data['date'], //photo
+            'value' => $data['value'],
+            'description' => $data['description'],
+        ]); 
+
+        $file = $data['document_id']; 
+
+         if ($file->isValid()) {
+            $path= $file->store('documents');
+            $movement->update(['document_id' => basename($path) ]);
+            Storage::setVisibility($path, 'documents');
+        }
 
 
+        'account_id', 
+         
+         
+         
+        'start_balance', 
+        'end_balance',
+        
+        
+    ];
 
-
+    	return $movement;
     }
 
     public function store(){
+    	$this->authorize('create', Movement::class);
+
+    	$movement = new movement();
+    	$movement->fill($request->all());
+    	$movement->save();
+
+    	return redirect()
+    	->route('movements')
+    	->with('success','Movement added successfully');
+    }
 
 
+    public function edit(Movement $movement){
+    	$this->authorize('update', $movement);
+        return view('movements.edit', compact('movement'));
 
 
     }
 
-    public function edit(){
+    public function update(Request $request, Movement $movement)
+    {
+    	$this->authorize('update', $movement);
+        $this->validate($request, [
+            
+        ]);
+        $movement->fill($request->except('password'));
+        $user->save();
 
-
-
-    }
-
-    public function update(){
+        return redirect()
+            ->route('movements')
+            ->with('success', 'Movement updated successfully!');
 
 
 
