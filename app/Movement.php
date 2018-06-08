84255ebdfa18b1525c32ef1f36740c50122a2f6c
PE60\marta <martacvcps@gmail.com>
2018-06-08 20:44:12 +0100

    .

--- app/Movement.php
+++ app/Movement.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movement extends Model
 {
-   
+   public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
